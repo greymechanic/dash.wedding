e65c959b42e08d9fa26738101b396b93ee576225
Andrew Kaye <notandrewkaye@gmail.com>
2021-08-18 21:32:56 -0700

    fix resize

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import woodside1 from "./images/woodside-1.jpg";
 import woodside2 from "./images/woodside-2.jpg";
 
 function App() {
-  let vh = window.innerHeight * 0.01;
+  var vh = window.innerHeight * 0.01,
+    delay = 250, // delay between calls
+    throttled = false; // are we currently throttled?
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  function setHeight() {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  window.addEventListener('resize', function () {
+    // only run if we're not throttled
+    if (!throttled) {
+      setHeight();
+      throttled = true;
+      setTimeout(() => {
+        throttled = false;
+      }, delay);
+    }
+  });
+
+  setHeight();
   return (
     <div className="home page">
       <header className="marquee">
