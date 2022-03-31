15fd234f1615fa063cce36686f4cb0aa564be5e9
Andrew Kaye <notandrewkaye@gmail.com>
2022-03-31 21:07:30 +0200

    details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,56 @@
 
 function Details() {
   return (
-    <div className="main bright">
+    <div className="container bright">
       <section>
         <h2>Details</h2>
+        <div className="content">
+          <div className="detailsTable">
+            <div className="row">
+              <div className="cell label">
+                <p><b>WHEN</b></p>
+              </div>
+              <div className="cell detail">
+                <p>June 4, 2022 @ 4PM</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="cell label">
+                <p><b>WHERE</b></p>
+              </div>
+              <div className="cell detail">
+                <p><a href="https://goo.gl/maps/H7yEEogJ2i4WZmRh8" target="_blank">1151 Woodside Rd Berkeley, CA 94708</a></p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="cell label">
+                <p><b>DRESS</b></p>
+              </div>
+              <div className="cell detail">
+                <p>Anything goes, <a href="#dress-code" target="_blank">really</a>!</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="cell label">
+                <p><b>GIFTS</b></p>
+              </div>
+              <div className="cell detail">
+              <p><a href="#" target="_blank">View our registry</a></p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="cell label">
+                <p><b>RSVP</b></p>
+              </div>
+              <div className="cell detail">
+                <p>Please <a href="#" target="_blank">RSVP</a> by May 1</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="rsvp">
+          <a href="#" type="button" target="_blank">RSVP</a>
+        </div>
       </section>
     </div>
   );
